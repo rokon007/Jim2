69c3324b6e5b4aef01670eb47e47384a296964de
rokon007 <rezaulkrm85@gmail.com>
2022-05-15 21:23:40 +0600

    er

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -375,7 +375,8 @@ class ProductController extends Controller
            //     -> get();
 		//$Total = DB::table('sales_orders')->where('invoice' , $pass)->sum('amount');
 		// -> get();
-		$Total =sales_order::where('invoice',$pass)->sum('amount');
+		//$Total =sales_order::where('invoice',$pass)->sum('amount');
+		$Total =sales_order::select(DB::raw('sum(cast(amount as double precision))'))->where('invoice', $pass)->get()
     
 	 return redirect()->back()->with(['pass' => $pass,'Total' => $Total,'cart_order' => $cart_order, 'view_cart' => 'view_cart']);
 	 }
