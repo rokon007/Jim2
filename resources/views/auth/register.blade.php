b42179cb735d2eb51e0d4c2e2be3f80794fe7e91
rokon007 <rezaulkrm85@gmail.com>
2022-06-09 21:03:40 +0600

    er

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -162,7 +162,7 @@
                       
                         <div class="form-group">
                             <label>Name</label>
-                            <input type="text" class="form-control@error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus >
+                            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus >
                                @error('name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -171,7 +171,7 @@
                         </div>
                         <div class="form-group">
                             <label>Email ID</label>
-                            <input type="email" class="form-control@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                            <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
                                @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -184,7 +184,7 @@
                         </div>
                         <div class="form-group">
                             <label>Password  min:8 Character</label>
-                            <input type="password" class="form-control@error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                            <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
